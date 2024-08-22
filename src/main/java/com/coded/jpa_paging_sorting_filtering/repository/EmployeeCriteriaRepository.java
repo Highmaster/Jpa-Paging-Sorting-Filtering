dc01413c6d1fc23c9601df3924bb76d949058727
Highmaster <nzechideraann@gmail.com>
2024-08-22 20:20:50 +0100

    initializing the entity manager through the constructor

--- src/main/java/com/coded/jpa_paging_sorting_filtering/repository/EmployeeCriteriaRepository.java
+++ src/main/java/com/coded/jpa_paging_sorting_filtering/repository/EmployeeCriteriaRepository.java
@@ -2,10 +2,20 @@ package com.coded.jpa_paging_sorting_filtering.repository;
 
 
 import com.coded.jpa_paging_sorting_filtering.model.Employee;
+import com.coded.jpa_paging_sorting_filtering.model.EmployeePage;
+import com.coded.jpa_paging_sorting_filtering.model.EmployeeSearchCriteria;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class EmployeeCriteriaRepository {
@@ -16,9 +26,5 @@ public class EmployeeCriteriaRepository {
     public EmployeeCriteriaRepository(EntityManager entityManager) {
         this.entityManager = entityManager;
         this.criteriaBuilder = entityManager.getCriteriaBuilder();
-    }
-
-    public Page<Employee> findAllWithFilters() {
-
     }
 }
