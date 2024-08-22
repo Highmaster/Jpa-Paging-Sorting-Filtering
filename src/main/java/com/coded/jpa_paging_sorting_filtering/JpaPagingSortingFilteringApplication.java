65b4b93ddd268c2289711c0b7752927c9a526ded
Highmaster <nzechideraann@gmail.com>
2024-08-22 09:57:06 +0100

    first commit

---
+++ src/main/java/com/coded/jpa_paging_sorting_filtering/JpaPagingSortingFilteringApplication.java
@@ -0,0 +1,13 @@
+package com.coded.jpa_paging_sorting_filtering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPagingSortingFilteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPagingSortingFilteringApplication.class, args);
+	}
+
+}
