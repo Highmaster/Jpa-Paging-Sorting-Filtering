1c5863a6c9b7a55d74301c770a433ac850b8c77a
Highmaster <nzechideraann@gmail.com>
2024-08-22 22:11:39 +0100

    endpoint for retrieving employee

--- src/main/java/com/coded/jpa_paging_sorting_filtering/controller/EmployeeController.java
+++ src/main/java/com/coded/jpa_paging_sorting_filtering/controller/EmployeeController.java
@@ -1,6 +1,13 @@
 package com.coded.jpa_paging_sorting_filtering.controller;
 
+import com.coded.jpa_paging_sorting_filtering.model.Employee;
+import com.coded.jpa_paging_sorting_filtering.model.EmployeePage;
+import com.coded.jpa_paging_sorting_filtering.model.EmployeeSearchCriteria;
 import com.coded.jpa_paging_sorting_filtering.service.EmployeeService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +19,12 @@ public class EmployeeController {
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Employee>> getEmployee(EmployeePage employeePage,
+                                                      EmployeeSearchCriteria employeeSearchCriteria) {
+        return new ResponseEntity<>(employeeService.getEmployees(employeePage, employeeSearchCriteria),
+                HttpStatus.OK);
     }
 }
